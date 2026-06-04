32ede35f1d88da2ce368fa0d5cf2ef2d5f0f88b1
rikitikitavi1488322-lab <rikitikitavi1488322@gmail.com>
2026-06-04 16:46:44 +0300

    Add files via upload

---
+++ raspred.py
@@ -0,0 +1,27 @@
+import clas
+
+def detect_d(c1, c2, sp_sloy):
+    """Ищет один самый близкий слой с учетом индивидуального порога этого слоя"""
+    r1, g1, b1 = c2
+    best_layer = None
+    min_found_d2 = float('inf')
+
+    for s in sp_sloy:
+        r0, g0, b0 = s.rgb
+        d2 = (r1 - r0)**2 + (g1 - g0)**2 + (b1 - b0)**2
+        if d2 < min_found_d2:
+            min_found_d2 = d2
+            best_layer = s
+
+    if best_layer and min_found_d2 < best_layer.max_d2:
+        best_layer.sp_pix.add(c1)  # .add() вместо .append(), так как sp_pix — это set
+
+def detect_clas(c, rgb_color, sp_sloy):
+    noviy_sloy = clas.Sloy(name=c, rgb=rgb_color, sp_pix=[], vozrast="???", description="")
+    sp_sloy.append(noviy_sloy)
+
+def create_custom_layer(name, rgb_color, sp_sloy, min_d2=850):
+    """Создает новый пользовательский пустой слой"""
+    noviy_sloy = clas.Sloy(name=name, rgb=rgb_color, sp_pix=[], vozrast="???", description="", max_d2=min_d2)
+    sp_sloy.append(noviy_sloy)
+    return noviy_sloy
